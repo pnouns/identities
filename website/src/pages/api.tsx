b7eaa518e9c7fbbc2f641bc046e26c0f31e0e4a5
Flyyn <fly@pnouns.fyi>
2021-07-08 03:04:31 +0000

    Add header to API docs.

--- website/src/pages/api.tsx
+++ website/src/pages/api.tsx
@@ -1,14 +1,18 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
 import { RedocStandalone } from 'redoc';
+import { Header } from "../components/Header";
 
 const API: React.FC<{}> = () => {
   return (
-    <Box bg="white">
-      <RedocStandalone specUrl="/api.yaml" options={{
-        jsonSampleExpandLevel: 2,
-      }} />
-    </Box>
+    <>
+      <Header />
+      <Box bg="white">
+        <RedocStandalone specUrl="/api.yaml" options={{
+          jsonSampleExpandLevel: 2,
+        }} />
+      </Box>
+    </>
   );
 }
 
